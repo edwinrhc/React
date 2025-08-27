4c2ff88dd134760ff3e41a0ef49a8adbdbdb5e6e
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-08-26 23:22:33 -0500

    Iniciando React

--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {FirstStepsApp} from "./FirstStepsApp.tsx";
+import {MyAwesomeApp} from "./MyAwesomeApp.tsx";
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <FirstStepsApp/>
+        <MyAwesomeApp/>
+    </StrictMode>,
 )
