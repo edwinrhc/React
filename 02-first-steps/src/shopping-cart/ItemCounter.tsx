57da76137162af4b29270df375261055a753280e
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-08-27 23:20:38 -0500

    feat(ItemCounter): useState

--- 02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,5 +1,48 @@
-export const ItemCounter = () => {
+import {useState} from "react";
+
+
+
+import './ItemCounter.css';
+
+
+export interface Props {
+    name: string;
+    quantity?: number
+}
+
+
+export const ItemCounter = ({name, quantity = 1 }: Props) => {
+
+     const [count, setCount] = useState(quantity);
+
+     const handleAdd = () => {
+         setCount(count + 1);
+     }
+     const handleSubtract = () => {
+         if(count === 1) return;
+         setCount(count - 1);
+     }
+
     return (
-        <div>ItemCounter</div>
+        <section
+            className="item-row"
+        //     style={{
+        //     display: 'flex',
+        //     alignItems: 'center',
+        //     gap: 10,
+        //     marginTop: 20,
+        // }}
+        >
+            <span
+                className="item-text"
+                style={{
+                    color: count === 1 ? 'red' : 'black',
+                }}
+            >{name}</span>
+            <button
+                onClick={handleAdd}>+1</button>
+            <span>{count}</span>
+            <button onClick={handleSubtract}>-1</button>
+        </section>
     )
 }
