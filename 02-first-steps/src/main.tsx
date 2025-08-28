d05d883d7ec3ff0a6a82f85d6e96de3a3709a26c
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-08-27 23:20:11 -0500

    Estilos

--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -1,12 +1,12 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {FirstStepsApp} from "./FirstStepsApp.tsx";
-import {MyAwesomeApp} from "./MyAwesomeApp.tsx";
+// import {MyAwesomeApp} from "./MyAwesomeApp.tsx";
 
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <FirstStepsApp/>
-        <MyAwesomeApp/>
+        {/*<MyAwesomeApp/>*/}
     </StrictMode>,
 )
