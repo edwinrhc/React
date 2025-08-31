11f663f2e33038cf9cd53f48774cc6369d56287e
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-08-30 22:05:10 -0500

    test(math-helper): agregar test math helpers

--- 02-first-steps/src/helpers/math.helper.test.ts
+++ 02-first-steps/src/helpers/math.helper.test.ts
@@ -1,18 +1,43 @@
-import {expect, test} from "vitest";
-import {add} from "./math.helper.ts";
+import {describe, expect, test} from "vitest";
+import {add, multiply, substract} from "./math.helper.ts";
 
+describe('add', () => {
 
-test('should add two positive numbers', () => {
+    test('should add two positive numbers', () => {
 
-    //! 1. Arrange
-    const a = 1;
-    const b = 2;
+        //! 1. Arrange
+        const a = 23;
+        const b = 2;
 
-    //! 2. Act
-    const result = add(a, b);
+        //! 2. Act
+        const result = add(a, b);
 
-    //! 3. Assert
-    console.log({result});
-    expect(result).toBe(4); // Aqui esperamos que el resultado sea 4
+        //! 3. Assert
+        console.log({result});
+        expect(result).toBe(a + b); // Aqui esperamos que el resultado sea 4
+    });
+})
 
+describe('substract', () => {
+    test('should substract numbers', () => {
+        //Arrange
+        const a = 23;
+        const b = 2;
+
+        const result = substract(a, b);
+
+        expect(result).toBe(a-b);
+    })
 });
+
+describe('multiply', () => {
+    test('should multiply numbers', () => {
+        //Arrange
+        const a = 2;
+        const b = 4;
+
+        const result = multiply(a, b);
+        console.log(`Resultado de multiplicar ${a} * ${b} = ${result}`)
+        expect(result).toBe(a * b);
+    })
+})
