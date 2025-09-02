f822a101ed578a41d108809767507b1b9571ad6a
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-09-01 23:13:16 -0500

    test(ItemCounterTest): -Should change  to red when count is 1 -Should change to blacak when count is greater than 1

--- 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
@@ -42,5 +42,23 @@ describe('ItemCounter', () => {
         const [ ,buttonSubtract ] = screen.getAllByRole('button');
         fireEvent.click(buttonSubtract);
         expect( screen.getByText('1')).toBeDefined();
+    });
+    // ----------------------------------------------------------------------------------------------------------------------
+    test('Should change to red when count is 1', ()=> {
+        const quantity = 1;
+        const name = 'Test item';
+        render(<ItemCounter name={name} quantity={ quantity } />);
+        const itemText = screen.getByText(name);
+        expect(itemText.style.color).toBe('red');
+
+    });
+    // ----------------------------------------------------------------------------------------------------------------------
+    test('should change to black when count is greater than 1', ()=> {
+        const quantity = 2;
+        const name = 'Test item';
+        render(<ItemCounter name={name} quantity={ quantity } />);
+        const itemText = screen.getByText(name);
+        expect(itemText.style.color).toBe('black');
+
     })
 });
