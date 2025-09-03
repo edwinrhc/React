f5150456f149b3c3afe40cb2b86016f91fd95cf3
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-09-02 23:26:19 -0500

    feat(GifApp): Implementando gif.mock

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -1,18 +1,19 @@
+import {mockGifs} from "./mock-data/gifs.mock.ts";
 
 export const GifsApp = () => {
-    return(
+    return (
         <>
-        {/*header*/}
+            {/*header*/}
             <div className="content-center">
                 <h1>Buscador de Gifs</h1>
                 <p>Descubre y comparte el gif Perfecto</p>
             </div>
             {/* Search*/}
             <div className="search-container">
-                <input type="text" placeholder="Buscar Gifs" />
+                <input type="text" placeholder="Buscar Gifs"/>
                 <button>Buscar</button>
             </div>
-        {/*    Búsqueda previas*/}
+            {/*    Búsqueda previas*/}
             <div className="previous-searches">
                 <h2>Búsquedas previas</h2>
                 <ul className="previous-searches-list">
@@ -21,9 +22,19 @@ export const GifsApp = () => {
                     <li>Elden Ring</li>
                 </ul>
             </div>
-        {/*    Gif */}
+            {/*    Gif */}
             <div className="gifs-container">
-                
+                {
+                    mockGifs.map((gif) => (
+                        <div key={gif.id} className="gif-card">
+                            <img src={gif.url} alt={gif.title}/>
+                            <h3>{gif.title}</h3>
+                            <p>
+                                {gif.width} x {gif.height} (1.5mb)
+                            </p>
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
