893fea1e164e72b44b59553b4ed76f06978c8080
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-09-03 23:28:38 -0500

    feat(GifApp): Comunicar entre componentes Padre e hijo

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -4,8 +4,16 @@ import {SearchBar} from "./shared/components/SearchBar.tsx";
 import {PreviousSearches} from "./gifs/components/PreviousSearches.tsx";
 import {GifList} from "./gifs/components/GifList.tsx";
 import {mockGifs} from "./mock-data/gifs.mock.ts";
+import {useState} from "react";
 
 export const GifsApp = () => {
+
+    const [previousTerms, setPreviousTerms] = useState(['dragon ball z'])
+
+    const handleTermClicked = (term: string) => {
+        console.log({term});
+    }
+
     return (
         <>
             {/*header*/}
@@ -13,7 +21,9 @@ export const GifsApp = () => {
             {/* Search*/}
             <SearchBar placeHolder="Busca lo que quieras..." />
             {/*    Búsqueda previas */}
-            <PreviousSearches searches={['Goku', 'Vegeta', 'Trunks']} />
+            <PreviousSearches
+                searches={previousTerms}
+                onLabelClicked={(term:string) => handleTermClicked(term)}/>
 
             {/*    GifList */}
             <GifList  gifs={mockGifs}/>
